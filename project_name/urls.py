e9a95d6b01ea7ee6bdf68210820450319f4046b0
Pascal Pepe <pascal@padjana.com>
2018-09-21 00:12:22 +0200

    Make the project translatable

--- project_name/urls.py
+++ project_name/urls.py
@@ -18,10 +18,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
 ]
+
+urlpatterns += i18n_patterns(
+    path(_('admin/'), admin.site.urls),
+)
