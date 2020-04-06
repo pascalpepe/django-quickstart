fb90362dd89deae366cc23131dfaae0f1a2f805a
Pascal Pepe <contact@pascalpepe.fr>
2020-04-06 20:14:03 +0200

    Remove secret_key template variable

--- project_template/project_name/settings/dev.py
+++ project_template/project_name/settings/dev.py
@@ -9,7 +9,7 @@ from {{ project_name }}.settings.base import *  # noqa
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'not-safe-for-prod')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
